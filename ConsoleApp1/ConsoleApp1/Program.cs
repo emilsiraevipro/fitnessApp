06db5c264d5aa7bec825e319775d3ad667c69f95
emilsiraevipro <emil.siraevipro@gmail.com>
2026-05-03 02:52:02 +0300

    add Numbers

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -4,7 +4,21 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Numbers numbers = new Numbers();
+            foreach (int n in numbers)
+            {
+                Console.WriteLine(n);
+            }
+        }
+    }
+    class Numbers
+    {
+        public IEnumerator<int> GetEnumerator()
+        {
+            for (int i = 0; i < 6; i++)
+            {
+                yield return i * i;
+            }
         }
     }
 }
